c84ad229742b1f7682880724e471180de4141d92
ofirx <ofirx@post.bgu.ac.il>
2018-11-03 15:59:34 +0200

    3 something shit

--- src/Table.cpp
+++ src/Table.cpp
@@ -3,24 +3,61 @@
 //
 
 #include "../include/Table.h"
-//GetTableIndex return table index
-//boolean availible
-//vector dishes
-//vector of custoners
-//int T_id
-//final int seats
 #include <vector>
 using namespace std;
 
 class Table {
+
     public:
+
+    //fields
+
         int T_id; //id of a table
-        final int seats; //const number of seats per table
-        boolean available; //indicates whether a table is close/open
+        int capacity; //const number of seats per table, can i add a final over here if not existing on h file?
+        boolean open; //indicates whether a table is close/open
+        vector <Customer> customersList; //a list of customers per table
+        vector <OrderPair> orderList; //a list of orders per table
 
+    // a constructor
 
+        Table():capacity(t_capacity){
 
+        }
 
+    // a copy constructor
+
+    Table(const Table &other):capacity(other.t_capacity){
+
+    }
+
+    // a destructor
+
+    ~Table(){
+        }
+
+        //function that indicates whether a table is close/open
+        bool isOpen(){
+
+            return open;
+        }
+
+    int GetTableIndex (){
+
+      return T_id;
+
+    }
+
+//Table(int t_capacity);
+//    int getCapacity() const;
+//    void addCustomer(Customer* customer);
+//    void removeCustomer(int id);
+//    Customer* getCustomer(int id);
+//    std::vector<Customer*>& getCustomers();
+//    std::vector<OrderPair>& getOrders();
+//    void order(const std::vector<Dish> &menu);
+//    void openTable();
+//    void closeTable();
+//    int getBill();
 
 
 
