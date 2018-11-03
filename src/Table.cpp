67344b100e53a5ce1938f7c1078e2fe2cef24f17
ofirx <ofirx@post.bgu.ac.il>
2018-11-03 15:00:05 +0200

    did nothing special #1

--- src/Table.cpp
+++ src/Table.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "../include/Table.h"
+//GetTableIndex return table index
+//boolean availible
+//vector dishes
+//vector of custoners
+//int T_id
+//final int seats
+#include <vector>
+using namespace std;
+
+class Table {
+    public:
+        int T_id; //id of a table
+        final int seats; //const number of seats per table
+        boolean available; //indicates whether a table is close/open
+
+
+
+
+
+
+
+
+
+
+
+};
